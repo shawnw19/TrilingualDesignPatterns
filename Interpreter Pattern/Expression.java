6785a7a57a2acdd8708c6c317e4fc81b85614857
shawnw19 <58173551+shawnw19@users.noreply.github.com>
2020-01-19 16:19:55 +0800

    Add files via upload

---
+++ Interpreter Pattern/Expression.java
@@ -0,0 +1,12 @@
+package FindingFiles_Interpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Expression {
+    //variables here are final
+//    so the declaration below is wrong
+//    List results = new ArrayList();
+
+    List getResultsList();
+}
